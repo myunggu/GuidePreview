642ebb3ed9ab5cf38d9e191d5272e78520ab6a1e
myunggukim <myunggukim@myungguui-macbookpro.local>
2018-12-28 16:31:56 +0900

    Initial Commit

---
+++ GuidePreview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuidePreview
+//
+//  Created by Myunggu Kim on 28/12/2018.
+//  Copyright © 2018 myunggukim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
